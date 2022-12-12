b29dfe6c85729479f16bfcb14719da5b5b2d170d
Tuman8 <i-a-l-e-x@yandex.ru>
2022-12-13 02:32:59 +0700

    "Своя игра" задаем вопросы, начисляем баллы

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
     while check_answer() is False:
         print(data_output_screen())  # выводим на экран наши темы и цены
         user_input = input('Выбирайте категорию вопроса и цену:\n')
-        # user_input = 'животные 300'  # временная строчка (после удалим)
 
         if user_selection(user_input) is False:
             print('Введите категорию и цену через пробел!')
@@ -14,10 +13,11 @@ if __name__ == '__main__':
             user_category, user_price = user_selection(user_input)
             if is_question(user_category, user_price) is False:
                 print(f'Вы уже использовали данное задание.')
-
             else:
-                user_answer = input(is_question(user_category, user_price))  # проверяем наличие темы и цены, а затем
-                # получаем ответ от пользователя
-                print(checking_user_answer(user_answer, user_category, user_price))  # проверяем ответ пользователя
-                # и начисляем баллы
+                user_answer = input(is_question(user_category, user_price))
+                # проверяем наличие темы и цены, а затем получаем ответ от пользователя
+                print(checking_user_answer(user_answer, user_category, user_price))
+                # проверяем ответ пользователя и начисляем баллы
     print('Вопросов больше нет')
+
+    #TODO надо подсчитать очки и сделать сброс JSON-а
