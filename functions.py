a1279d07a470050817646fa71d7b9f01d144dc8d
Tuman8 <i-a-l-e-x@yandex.ru>
2022-12-13 02:27:58 +0700

    задаем вопросы, начисляем баллы

--- functions.py
+++ functions.py
@@ -8,10 +8,6 @@ def read_questions():
     return data
 
 
-# print(type(read_questions()), read_questions())
-# print(read_questions())
-
-
 def check_answer():
     """Проверяем на все вопросы мы ответили или нет"""
     data = read_questions()
@@ -23,9 +19,6 @@ def check_answer():
         return False
     else:
         return True
-
-
-# print(check_answer())
 
 
 def data_output_screen():
@@ -44,19 +37,61 @@ def data_output_screen():
     return info_game[:-1]
 
 
-# print(data_output_screen())
-
-
 def user_selection(user_input):
     """Выбор пользователем категории и цены категории"""
-    if user_input.strip().count(' ') == 1:
+    count_spaces = user_input.strip().count(' ')
+    if count_spaces > 1:
+        return False
+    elif count_spaces == 1:
         user_input = user_input.strip().title().split()
         return user_input[0], int(user_input[1])
-    else:
-        return 'Введите категорию и цену через пробел!'
 
 
 def is_question(user_category, user_price):
     """Проверка наличия выбранной категории и цены вопроса"""
-    pass
+    data = read_questions()
+    all_category = list(data.keys())
+    if user_category not in all_category:
+        print(f"Категории: {user_category} не существует!")
 
+    for category, category_variables in data.items():
+        if str(user_price) not in list(category_variables.keys()):
+            print(f"Такой цены: \"{user_price}\" не существует!")
+            break
+        elif user_category == category:
+            for price, price_variables in category_variables.items():
+                if user_price == int(price) and price_variables['asked'] is False:
+                    return f'Слово \"{price_variables["question"]}\" в переводе означает: '
+                elif user_price == int(price) and price_variables['asked'] is True:
+                    return False
+
+
+# print(is_question('Животные', 300))
+
+TOTAL_SCORE = 0
+
+
+def checking_user_answer(user_answer, user_category, user_price):
+    """Проверяем ответ пользователя"""
+    global TOTAL_SCORE
+    data = read_questions()
+    for category, category_variables in data.items():
+        if category == user_category:
+            for price, price_variables in category_variables.items():
+                if price == str(user_price):
+                    # print(price_variables)
+                    price_variables["asked"] = True
+                    # print(price_variables)
+                    if user_answer.strip().lower() == price_variables['answer']:
+                        TOTAL_SCORE += int(price)
+                        result = f'Верно, +{price}. Ваш счет = {TOTAL_SCORE}'
+                    else:
+                        TOTAL_SCORE -= int(price)
+                        result = f'Неверно, на самом деле \"{price_variables["answer"]}\". –{price}. Ваш счет = {TOTAL_SCORE}'
+    raw_json = json.dumps(data, ensure_ascii=False, indent=4)
+    with open('questions.json', 'w', encoding='utf-8') as file:
+        file.write(raw_json)
+    return result
+
+
+# print(checking_user_answer('Акула', 'Животные', 200))
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from functions import data_output_screen, user_selection
-
+from functions import data_output_screen, user_selection, is_question, checking_user_answer, check_answer
 
 if __name__ == '__main__':
 
-    print(data_output_screen())
-    # user_input = input('Выбирайте категорию вопроса и цену:\n')
-    user_input = ' ЕДА 100  '
-    user_category, user_price = user_selection(user_input)
-    print(user_category)
-    print(user_price)
+    while check_answer() is False:
+        print(data_output_screen())  # выводим на экран наши темы и цены
+        user_input = input('Выбирайте категорию вопроса и цену:\n')
+        # user_input = 'животные 300'  # временная строчка (после удалим)
+
+        if user_selection(user_input) is False:
+            print('Введите категорию и цену через пробел!')
+
+        elif user_selection(user_input):
+            user_category, user_price = user_selection(user_input)
+            if is_question(user_category, user_price) is False:
+                print(f'Вы уже использовали данное задание.')
+
+            else:
+                user_answer = input(is_question(user_category, user_price))  # проверяем наличие темы и цены, а затем
+                # получаем ответ от пользователя
+                print(checking_user_answer(user_answer, user_category, user_price))  # проверяем ответ пользователя
+                # и начисляем баллы
+    print('Вопросов больше нет')
