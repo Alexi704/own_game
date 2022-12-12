8b23bccf49700abe6a22f875088d118ad5c8a1fd
Tuman8 <i-a-l-e-x@yandex.ru>
2022-12-12 23:33:59 +0700

    init

--- functions.py
+++ functions.py
@@ -12,11 +12,27 @@ def read_questions():
 # print(read_questions())
 
 
+def check_answer():
+    """Проверяем на все вопросы мы ответили или нет"""
+    data = read_questions()
+    all_answer = []
+    for _, price in data.items():
+        for _, price_variables in price.items():
+            all_answer.append(price_variables['asked'])
+    if False in all_answer:
+        return False
+    else:
+        return True
+
+
+# print(check_answer())
+
+
 def data_output_screen():
     """Выводим на экран категорию вопроса и цену вопроса"""
     data = read_questions()
     max_len_word_category = max(len(w) for w in list(data.keys()))  # находим длину самого длинного слова
-    info_game = ''
+    info_game = '\n'
     for category, category_variables in data.items():
         info_game += category.rjust(max_len_word_category)
         for price, price_variables in category_variables.items():
@@ -25,5 +41,22 @@ def data_output_screen():
             else:
                 info_game += ''.rjust(5)
         info_game += '\n'
-    return info_game
+    return info_game[:-1]
+
+
 # print(data_output_screen())
+
+
+def user_selection(user_input):
+    """Выбор пользователем категории и цены категории"""
+    if user_input.strip().count(' ') == 1:
+        user_input = user_input.strip().title().split()
+        return user_input[0], int(user_input[1])
+    else:
+        return 'Введите категорию и цену через пробел!'
+
+
+def is_question(user_category, user_price):
+    """Проверка наличия выбранной категории и цены вопроса"""
+    pass
+
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from functions import data_output_screen
+from functions import data_output_screen, user_selection
 
 
 if __name__ == '__main__':
+
     print(data_output_screen())
+    # user_input = input('Выбирайте категорию вопроса и цену:\n')
+    user_input = ' ЕДА 100  '
+    user_category, user_price = user_selection(user_input)
+    print(user_category)
+    print(user_price)
